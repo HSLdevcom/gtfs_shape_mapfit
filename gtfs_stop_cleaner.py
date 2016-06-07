02b2ff0ae6b67af78e12572e352268891a50b6a2
rrudd <raoul.udd@netlight.com>
2016-06-07 13:30:17 +0300

    don't handle railway stations

--- gtfs_stop_cleaner.py
+++ gtfs_stop_cleaner.py
@@ -10,28 +10,27 @@ import csv
 # Stolen from http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
 def haversine((lat1, lon1), (lat2, lon2)):
     """
-    Calculate the great circle distance between two points 
+    Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    c = 2 * asin(sqrt(a))
 
     # 6367 km is the radius of the Earth
     km = 6367 * c
     return km*1000.0
-	
+
 
 def read_osm_stops(osmfile):
 	stop_coords = defaultdict(list)
 	def handle_node(node):
 		if (node[1].get('highway') != 'bus_stop' and
-                    node[1].get('railway') != 'station' and
                     node[1].get('railway') != 'tram_stop'):
                     return
 		if 'ref' not in node[1]: return
