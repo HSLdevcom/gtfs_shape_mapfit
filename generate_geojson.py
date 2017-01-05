124f831a9e9491c559a5601692e7029c8a67d89f
Sebastian <sebastian.hognabba@gmail.com>
2017-01-05 14:11:53 +0200

    Changed variable names from camelCase to lowercase_with_underscore

--- generate_geojson.py
+++ generate_geojson.py
@@ -6,54 +6,54 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 def generate_geojson(stop_file, shapefit_file, shape_file_original, shape_file_fitted):
-	stopFeatures = []
-	shapeFeatures = []
+	stop_features = []
+	shape_features = []
 	for row in open(stop_file):
 		row = row.split(';')
-		errorType = row[0]
-		if (errorType.startswith("Huge error") or errorType.startswith("No OSM stop")):
-			stopId = row[1].split(':')[1].lstrip()
-			coordinatesJore = ast.literal_eval(row[2])
-			featureJore = '{"type": "Feature", "geometry": {"type": "Point","coordinates": %s}, "properties": {"stopId": "%s", "errorType": "%s", "source": "JORE" }}'%(coordinatesJore, stopId, errorType)
-			stopFeatures.append(featureJore)
-			if (errorType.startswith("Huge error")):
-				coordinatesOsm = ast.literal_eval(row[3])
-				featureOsm = '{"type": "Feature", "geometry": {"type": "Point","coordinates": %s},"properties": {"stopId": "%s", "errorType": "%s", "source": "OSM" }}'%(coordinatesOsm, stopId, errorType)
-				stopFeatures.append(featureOsm)
+		error_type = row[0]
+		if (error_type.startswith("Huge error") or error_type.startswith("No OSM stop")):
+			stop_id = row[1].split(':')[1].lstrip()
+			coordinates_jore = ast.literal_eval(row[2])
+			feature_jore = '{"type": "Feature", "geometry": {"type": "Point","coordinates": %s}, "properties": {"stop_id": "%s", "error_type": "%s", "source": "JORE" }}'%(coordinates_jore, stop_id, error_type)
+			stop_features.append(feature_jore)
+			if (error_type.startswith("Huge error")):
+				coordinates_osm = ast.literal_eval(row[3])
+				feature_osm = '{"type": "Feature", "geometry": {"type": "Point","coordinates": %s},"properties": {"stop_id": "%s", "error_type": "%s", "source": "OSM" }}'%(coordinates_osm, stop_id, error_type)
+				stop_features.append(feature_osm)
 	
 	for row in open(shapefit_file):
 		row = row.split(';')
-		errorType = row[0]
-		if (errorType.startswith("Probably bad fit") or errorType.startswith("Outliers found")):
-			routeId = row[1].split(':')[1].lstrip()
-			coordinatesJore = find_coordinates(shape_file_original, routeId)
-			coordinatesOsm = find_coordinates(shape_file_fitted, routeId)
-			if (errorType.startswith("Probably bad fit")):
+		error_type = row[0]
+		if (error_type.startswith("Probably bad fit") or error_type.startswith("Outliers found")):
+			route_id = row[1].split(':')[1].lstrip()
+			coordinates_jore = find_coordinates(shape_file_original, route_id)
+			coordinates_osm = find_coordinates(shape_file_fitted, route_id)
+			if (error_type.startswith("Probably bad fit")):
 				score = row[2].split(':')[1].lstrip()
-				scoreLimit = row[3].split(': ')[1].lstrip()
-				propertiesJore = '{"routeId": "%s", "errorType": "%s", "source": "JORE", "score" : "%s", "limit" : "%s"}'%(routeId, errorType, score, scoreLimit)
-				propertiesOsm = '{"routeId": "%s", "errorType": "%s", "source": "OSM", "score" : "%s", "limit" : "%s"}'%(routeId, errorType, score, scoreLimit)
+				score_limit = row[3].split(': ')[1].lstrip()
+				properties_jore = '{"route_id": "%s", "error_type": "%s", "source": "JORE", "score" : "%s", "limit" : "%s"}'%(route_id, error_type, score, score_limit)
+				properties_osm = '{"route_id": "%s", "error_type": "%s", "source": "OSM", "score" : "%s", "limit" : "%s"}'%(route_id, error_type, score, score_limit)
 			else:
 				outliers = row[2].split(':')[1]
-				propertiesJore = '{"routeId": "%s", "errorType": "%s", "source": "JORE", "outliers" : "%s"}'%(routeId, errorType, outliers)
-				propertiesOsm = '{"routeId": "%s", "errorType": "%s", "source": "OSM", "outliers" : "%s"}'%(routeId, errorType, outliers)
-			featureJore = '{"type": "Feature", "geometry": {"type": "LineString", "coordinates": %s}, "properties": %s}'%(coordinatesJore, propertiesJore)
-			featureOsm = '{"type": "Feature", "geometry": {"type": "LineString", "coordinates": %s}, "properties": %s}'%(coordinatesOsm, propertiesOsm)
-			shapeFeatures.append(featureJore)
-			shapeFeatures.append(featureOsm)
-	allFeatures = stopFeatures + shapeFeatures
-	featureCollection = '{"type": "FeatureCollection", "features": [%s]}'%(", ".join([feature.decode('utf8') for feature in allFeatures]))
-	print >>sys.stderr, "%s"%(featureCollection)
+				properties_jore = '{"route_id": "%s", "error_type": "%s", "source": "JORE", "outliers" : "%s"}'%(route_id, error_type, outliers)
+				properties_osm = '{"route_id": "%s", "error_type": "%s", "source": "OSM", "outliers" : "%s"}'%(route_id, error_type, outliers)
+			feature_jore = '{"type": "Feature", "geometry": {"type": "LineString", "coordinates": %s}, "properties": %s}'%(coordinates_jore, properties_jore)
+			feature_osm = '{"type": "Feature", "geometry": {"type": "LineString", "coordinates": %s}, "properties": %s}'%(coordinates_osm, properties_osm)
+			shape_features.append(feature_jore)
+			shape_features.append(feature_osm)
+	all_features = stop_features + shape_features
+	feature_collection = '{"type": "feature_collection", "features": [%s]}'%(", ".join([feature.decode('utf8') for feature in all_features]))
+	print >>sys.stderr, "%s"%(feature_collection)
 
-def find_coordinates(file, routeId):
+def find_coordinates(file, route_id):
 	coordinates = []
 	for row in open(file):
 		row = row.split(',')
-		listRouteId = row[0]
-		if routeId == listRouteId:
-			coordinateLat = ast.literal_eval(row[1])
-			coordinateLon = ast.literal_eval(row[2])
-			coordinates.append([coordinateLon, coordinateLat])
+		list_route_id = row[0]
+		if route_id == list_route_id:
+			coordinate_lat = ast.literal_eval(row[1])
+			coordinate_lon = ast.literal_eval(row[2])
+			coordinates.append([coordinate_lon, coordinate_lat])
 	return coordinates
 
 if __name__ == '__main__':
