e4808f30d74c534d0e5d5e6ccbabc87f4da94a75
Sebastian <sebastian.hognabba@gmail.com>
2017-01-09 13:35:11 +0200

    Write geojson to stdout instead of stderr

--- fit_gtfs.bash
+++ fit_gtfs.bash
@@ -23,7 +23,8 @@ unzip $GTFS_FILE -d $TMP_DIR/orig_gtfs
 	2>&1 >$TMP_DIR/shapes.fitted.txt |tee $TMP_DIR/shapefit_stats.txt >&2
 "$TOOL_DIR"/filter_bad_fits.py $TMP_DIR/shapefit_stats.txt $TMP_DIR/shapes.fitted.txt $GTFS_DIR/shapes.txt \
 	2>&1 >$TMP_DIR/shapes.filtered.txt |tee $TMP_DIR/shapefit.log.txt >&2
-"$TOOL_DIR"/generate_geojson.py $TMP_DIR/stops.log.txt  $TMP_DIR/shapefit.log.txt $GTFS_DIR/shapes.txt $TMP_DIR/shapes.fitted.txt  2>&1 |tee $TMP_DIR/geojson.txt >&2
+"$TOOL_DIR"/generate_geojson.py $TMP_DIR/stops.log.txt $TMP_DIR/shapefit.log.txt $GTFS_DIR/shapes.txt $TMP_DIR/shapes.fitted.txt > $TMP_DIR/geojson.txt
+
 
 cp $TMP_DIR/shapes.filtered.txt $GTFS_DIR/shapes.txt
 cp $TMP_DIR/stops.fitted.txt $GTFS_DIR/stops.txt
